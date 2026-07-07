e1b118d98c1e8e3226c9062a649c2c1b10fa4f74
Priyanshu Kumar <kupriyanshu1212@gmail.com>
2026-07-07 12:09:38 +0530

    Renaming file

---
+++ basic 01.js
@@ -0,0 +1,10 @@
+const accountId = 9925
+let accountname = "Priyanshu Kumar"
+var accountBalance = 0.06
+accountaddress = "Bihar, Siwan, India"
+ console.table({accountId, accountname, accountBalance, accountaddress});
+ 
+/*
+Prefer not to use var, instead use let and const.
+Because of issue in block scope and functional scope.
+*/
