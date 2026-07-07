0dadb4f13d5384040d4f1af63b48fc49bf1bbb03
Priyanshu Kumar <kupriyanshu1212@gmail.com>
2026-07-07 19:03:31 +0530

    datatype in js

---
+++ 02 data types.js
@@ -0,0 +1,13 @@
+let name = "Priyanshu Kumar" // string data type
+let age = 22 // number data type
+let isloggedin = false // boolean data type
+let address = null // null data type
+let phoneNumber // undefined data type  
+// Javascript is the case sensitive language. So, name and Name are different variables.
+
+//string data type => ""
+// number data type => 2 to power of 53
+// boolean data type => true or false
+//null data type => standalone value that represents nothingness or no value
+//undifined data type => variable that has been declared but not assigned a value yet.
+//bigint data type => can represent integers with arbitrary precision. It can be used to store and manipulate large integers that exceed the range of the number data type. 
