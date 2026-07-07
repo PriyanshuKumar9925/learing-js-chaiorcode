bb9ddf90757152c12aa9179a99a8e02ed0df2e08
Priyanshu Kumar <kupriyanshu1212@gmail.com>
2026-07-07 19:41:03 +0530

    complete 2

--- 02 data types.js
+++ 02 data types.js
@@ -5,9 +5,18 @@ let address = null // null data type
 let phoneNumber // undefined data type  
 // Javascript is the case sensitive language. So, name and Name are different variables.
 
+console.table({name, age, isloggedin, address, phoneNumber})
+
 //string data type => ""
 // number data type => 2 to power of 53
 // boolean data type => true or false
 //null data type => standalone value that represents nothingness or no value
 //undifined data type => variable that has been declared but not assigned a value yet.
 //bigint data type => can represent integers with arbitrary precision. It can be used to store and manipulate large integers that exceed the range of the number data type. 
+// symbol data type => used to create unique identifiers for objects. It is a primitive data type that was introduced in ECMAScript 2015 (ES6). Symbols are often used as keys for object properties, and they can help prevent naming collisions in code.
+
+//object
+
+console.log(typeof undefined); // Type of undefined is undefined
+console.log(typeof null); // Type of null is object.
+
